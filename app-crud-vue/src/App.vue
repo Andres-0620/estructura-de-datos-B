b6fa1ab5d99a915a21d3640f6da2ce37a43a70a6
Andres Duran <afduranc@ufpso.edu.co>
2026-05-20 19:51:06 -0500

    Crud Vue Api

--- app-crud-vue/src/App.vue
+++ app-crud-vue/src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <div id="app">
-    <!-- Aquí se renderiza la vista activa -->
-    <router-view />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-export default { name: 'App' }
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
